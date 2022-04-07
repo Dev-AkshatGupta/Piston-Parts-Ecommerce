bfb7a1b814b72f4af10a0a04cf3c1da84cd4f333
akshatgupta105 <53713926+Dev-AkshatGupta@users.noreply.github.com>
2022-04-07 20:11:08 +0530

    Routes rectified

--- src/App.js
+++ src/App.js
@@ -23,8 +23,8 @@ const {authState:{token},authDispatch }=useAuthorization();
         <Route path="/" element={<LandingPage/>}/>
         <Route path="/products-page" element={<ProductsPage/>}/>
         <Route path="/wishlist-page" element={<PrivateRoute><WishlistPage/></PrivateRoute>}/>
-   {!token &&  <Route path="/signUp-Page" element={<SignUpPage/>}/>}
-   {   !token &&  <Route path="/logIn-Page" element={<LogInPage/>}/>}
+     <Route path="/signUp-Page" element={<SignUpPage/>}/>
+    <Route path="/logIn-Page" element={<LogInPage/>}/>
         <Route path="/cart-page" element={<PrivateRoute><CartPage/></PrivateRoute>}/>
       <Route path="/404-page" element={<Page404/>}/>
      {token && <Route path="/profile-page" element={<ProfilePage/>}/>}
